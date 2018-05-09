9489fce2e77ec4087d8ad04196ce84819c88d7fb
jbring <37533320+jbring@users.noreply.github.com>
2018-05-09 10:19:11 +0200

    Updated Controllers Request

--- HotelApi/Controllers/HotelsController.cs
+++ HotelApi/Controllers/HotelsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelApi.DbManager;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -10,27 +11,15 @@ namespace HotelApi.Controllers
     [Route("api/hotels")]
     public class HotelsController : Controller
     {
-        private readonly IHotelsRepository _hotelsRepository;
+        private readonly IRepository<> _hotelsRepository;
 
-        public HotelsController(IHotelsRepository hotelsRepository)
+        public HotelsController(IRepository<> hotelsRepository)
         {
             _hotelsRepository = hotelsRepository;
         }
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
 
-        
+
         [HttpGet, Route("DisplayAllHotels")]
         public IActionResult DisplayAllHotels()
         {
@@ -38,7 +27,7 @@ namespace HotelApi.Controllers
             return Ok(hotels);
         }
 
-        
+
         [HttpGet, Route("DisplayAllRegions")]
         public IActionResult DisplayAllRegions()
         {
@@ -53,17 +42,19 @@ namespace HotelApi.Controllers
             return Ok(message);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
+
+        [HttpDelete("DeleteRegion")]
         public IActionResult DeleteOneRegion(int id)
         {
             _hotelsRepository.DeleteRegion(id);
+            return Ok("Region Deleted");
+        }
+
+        [HttpDelete("RecedDatabase")]
+        public IActionResult RecedDatabase()
+        {
+            _hotelsRepository.RecedDatabase();
             return Ok("Region Deleted");
         }
     }
