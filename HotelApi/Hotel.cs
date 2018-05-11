57d0f7618887937990eb5fe38f18565bbd066dd4
jbring <37533320+jbring@users.noreply.github.com>
2018-05-11 14:48:18 +0200

    HotelClass

---
+++ HotelApi/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelApi
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RoomsAvaiable { get; set; }
+
+    }
+}
---
+++ HotelApi/Repository/HotelRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelApi.DbManager;
+
+namespace HotelApi
+{
+    public class HotelRepository : IRepository<Hotel>
+    {
+        private HotelContext context = new HotelContextFactory().CreateDbContext();
+        public Hotel Add(Hotel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel Get(Hotel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel Update(Hotel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel Remove(Hotel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hotel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hotel> Search(Hotel obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Reseed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
