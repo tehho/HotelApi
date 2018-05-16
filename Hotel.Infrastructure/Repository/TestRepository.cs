669ba82ebb839e85fcfc34fa80701631208d3597
jbring <37533320+jbring@users.noreply.github.com>
2018-05-16 11:19:17 +0200

    testcontroller

---
+++ Hotel.Infrastructure/Repository/TestRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Domain;
+using Hotel.Infrastructure.DbManager;
+
+namespace Hotel.Infrastructure.Repository
+{
+    public class TestRepository : IRepository<HotelRegion>
+    {
+        private readonly IDbManager _context;
+
+        public TestRepository(IDbManager context)
+        {
+            _context = context;
+        }
+        public HotelRegion Add(HotelRegion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HotelRegion Get(HotelRegion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HotelRegion Update(HotelRegion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HotelRegion Remove(HotelRegion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<HotelRegion> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<HotelRegion> Search(HotelRegion obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Reseed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HotelApi/Controllers/TestsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.Domain;
+using Hotel.Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApi.Controllers
+{
+    [Route("test/[controller]")]
+    public class TestsController :Controller
+    {
+       
+            private readonly IRepository<HotelRegion> _hotelRepository;
+            private readonly AppConfiguration _appConfiguration;
+
+
+            public TestsController(IRepository<HotelRegion> hotelsRepository, AppConfiguration appConfiguration)
+            {
+                _hotelRepository = hotelsRepository;
+                _appConfiguration = appConfiguration;
+            }
+
+            [HttpGet]
+            public IActionResult GetAllHotels()
+            {
+                return Ok(_hotelRepository.GetAll());
+            }
+
+        
+}
